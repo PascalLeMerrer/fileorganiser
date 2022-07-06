a5d7446cb2c7db6ef4abe5b98b38d4b71d4e27ce
Pascal Le Merrer <pascal.lemerrer@protonmail.com>
2022-07-06 14:35:52 +0200

    Ignore hidden files/dirs in subdirs -on Mac/Linux

--- hidden_notwindows.go
+++ hidden_notwindows.go
@@ -3,6 +3,10 @@
 // Source https://github.com/ayoisaiah/f2/ - MIT License
 package main
 
+import (
+    "strings"
+)
+
 const dotCharacter = 46
 const pathSeperator = "/"
 
@@ -10,5 +14,11 @@ const pathSeperator = "/"
 // the error is returned to match the signature of the Windows
 // version of the function.
 func isHidden(filename, baseDir string) (bool, error) {
-    return filename[0] == dotCharacter, nil
+    names :=  strings.Split(filename, pathSeperator)
+    for _, name := range(names) {
+        if strings.HasPrefix(name, ".") {
+            return true, nil
+        }
+    }
+    return false, nil
 }
